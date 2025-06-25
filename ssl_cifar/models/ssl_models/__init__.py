ae2340ba2bdf377a321c9c34a6ba46aa0db530c9
Jeff Wiroj <pasawee2@gmail.com>
2025-06-24 17:08:18 -0700

    Added Barlow Twins

--- ssl_cifar/models/ssl_models/__init__.py
+++ ssl_cifar/models/ssl_models/__init__.py
@@ -1,6 +1,9 @@
+from .barlow_twins import BarlowTwins
 from .simsiam import SimSiam
 
 
 def get_ssl_model(model_name="simsiam", **kwargs):
     if model_name == "simsiam":
         return SimSiam(**kwargs)
+    elif model_name == "barlow_twins":
+        return BarlowTwins(**kwargs)
---
+++ ssl_cifar/models/ssl_models/barlow_twins.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+
+class BarlowTwins(nn.Module):
+    def __init__(self, backbone, lambd):
+        super().__init__()
+        self.lambd = lambd
+        self.backbone = backbone
+        self.proj_mlp = nn.Sequential(
+            nn.Linear(512, 8192, bias=False),
+            nn.BatchNorm1d(8192),
+            nn.ReLU(),
+            nn.Linear(8192, 8192, bias=False),
+            nn.BatchNorm1d(8192),
+            nn.ReLU(),
+            nn.Linear(8192, 8192, bias=False),
+        )
+
+    def forward(self, x1, x2):
+        # x of shape: [B,3,32,32]
+
+        z1, z2 = self.encode(x1), self.encode(x2)  # shape [B,8192]
+        z1 = (z1 - z1.mean(0, keepdim=True)) / (z1.std(0, keepdim=True) + 1e-8)
+        z2 = (z2 - z2.mean(0, keepdim=True)) / (z2.std(0, keepdim=True) + 1e-8)
+        B, D = z1.shape
+
+        C = z1.T @ z2 / B
+        diag_c = torch.diag(C)
+
+        on_diag_loss = (diag_c - 1).pow(2).sum()
+        off_diag_loss = (C.pow(2).sum() - diag_c.pow(2).sum()) * self.lambd
+
+        loss = on_diag_loss + off_diag_loss
+
+        return z1, loss
+
+    def encode(self, x):
+        return self.proj_mlp(self.backbone(x))
